7c25ec8c27fa909593bbe9888ae063eb8b0fbbab
juanmartin1892 <juanmartin1892@gmail.com>
2018-02-24 12:55:54 +0100

    Add google map

--- resources/views/homepage/index.js
+++ resources/views/homepage/index.js
@@ -6,55 +6,70 @@ var request = require('superagent');
 var header = require('../header');
 var axios = require('axios');
 
-page('/', header, asyncLoad, function (ctx, next) {
-  title('Platzigram');
-  var main = document.getElementById('main-container');
+page('/', header, function(ctx, next) {
+    title('Platzigram - Signin');
+    var main = document.getElementById('main-container');
+    empty(main).appendChild(template());
+});
 
-  empty(main).appendChild(template(ctx.pictures));
-})
-
-function loadPictures(ctx, next) {
-  request
-    .get('/api/pictures')
-    .end(function (err, res) {
-      if (err) return console.log(err);
-
-      ctx.pictures = res.body;
-      next();
-    })
+window.initMap = function() {
+    var uluru = {
+        lat: 40.3488673,
+        lng: -3.6968806
+    };
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: uluru
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
 }
 
-function loadPicturesAxios(ctx, next) {
-  axios
-    .get('/api/pictures')
-    .then(function (res) {
-      ctx.pictures = res.data;
-      next();
-    })
-    .catch(function (err) {
-      console.log(err);
-    })
-}
-
-function loadPicturesFetch(ctx, next) {
-  fetch('/api/pictures')
-    .then(function (res) {
-      return res.json();
-    })
-    .then(function (pictures) {
-      ctx.pictures = pictures;
-      next();
-    })
-    .catch(function (err) {
-      console.log(err);
-    })
-}
-
-async function asyncLoad(ctx, next) {
-  try {
-    ctx.pictures = await fetch('/api/pictures').then(res => res.json());
-    next();
-  } catch (err) {
-    return console.log(err);
-  }
-}
+//
+// function loadPictures(ctx, next) {
+//   request
+//     .get('/api/pictures')
+//     .end(function (err, res) {
+//       if (err) return console.log(err);
+//
+//       ctx.pictures = res.body;
+//       next();
+//     })
+// }
+//
+// function loadPicturesAxios(ctx, next) {
+//   axios
+//     .get('/api/pictures')
+//     .then(function (res) {
+//       ctx.pictures = res.data;
+//       next();
+//     })
+//     .catch(function (err) {
+//       console.log(err);
+//     })
+// }
+//
+// function loadPicturesFetch(ctx, next) {
+//   fetch('/api/pictures')
+//     .then(function (res) {
+//       return res.json();
+//     })
+//     .then(function (pictures) {
+//       ctx.pictures = pictures;
+//       next();
+//     })
+//     .catch(function (err) {
+//       console.log(err);
+//     })
+// }
+//
+// async function asyncLoad(ctx, next) {
+//   // try {
+//   //   ctx.pictures = await fetch('/api/pictures').then(res => res.json());
+//   //   next();
+//   // } catch (err) {
+//   //   return console.log(err);
+//   // }
+// }
--- resources/views/homepage/template.js
+++ resources/views/homepage/template.js
@@ -3,16 +3,13 @@ var layout = require('../layout');
 var picture = require('../picture-card');
 
 module.exports = function (pictures) {
-  var el = yo`<div class="container timeline">
-    <div class="row">
-      <div class="col s12 m10 offset-m1 l6 offset-l3">
-        ${pictures.map(function (pic) {
-          return picture(pic);
-        })}
+  var el = yo`<div class="mainInfo">
+    <div class="row mainInfoMap">
+      <div class="col s12 offset-m1 l6 offset-l3 mainInfoMap">
+        <div id="map"></div>
       </div>
     </div>
   </div>`;
 
   return layout(el);
 }
-
